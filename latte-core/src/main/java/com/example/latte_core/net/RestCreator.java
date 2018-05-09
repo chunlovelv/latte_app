d53430fd5206261edf4d77f6f3fda9e717d3acc5
john <lichun575217834@163.com>
2018-05-09 17:58:52 +0800

    2018-5-9日学习笔记上传

--- latte-core/src/main/java/com/example/latte_core/net/RestCreator.java
+++ latte-core/src/main/java/com/example/latte_core/net/RestCreator.java
@@ -10,8 +10,6 @@ import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RestCreator {
-
-
     public static RestService getRestService(){
         return RestServiceHolder.REST_SERVICE;
     }
