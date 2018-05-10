fff9f5bdeff08ee4496477dff397b1bbdc541269
john <lichun575217834@163.com>
2018-05-10 18:05:17 +0800

    添加了加载动画库

--- app/src/main/java/com/example/john/latte_ec/TestDelegate.java
+++ app/src/main/java/com/example/john/latte_ec/TestDelegate.java
@@ -4,15 +4,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.latte_core.animation.AVLoadingIndicators;
 import com.example.latte_core.delegates.LatteDelegate;
 import com.example.latte_core.net.HttpMethod;
 import com.example.latte_core.net.RestClientBuilder;
 import com.example.latte_core.net.callback.IErrorCallback;
 import com.example.latte_core.net.callback.ISuccessCallBack;
+import com.wang.avi.AVLoadingIndicatorView;
 
 public class TestDelegate extends LatteDelegate {
+
+    AVLoadingIndicatorView avloadingview;
+
     @Override
     protected void onBinderView(Bundle savedInstanceState, View rootView) {
+
+        avloadingview = rootView.findViewById(R.id.avloadingview);
+        avloadingview.setIndicator(AVLoadingIndicators.BallClipRotateIndicator.name());
+        avloadingview.show();
         new RestClientBuilder()
                 .url("https://chuanke.baidu.com/")
                 .method(HttpMethod.GET)
