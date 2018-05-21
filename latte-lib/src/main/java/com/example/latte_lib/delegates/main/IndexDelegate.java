8f3b57083ae3246fb53471e22d5d011ba8e9f169
Gogs <gogs@fake.local>
2018-05-22 07:37:35 +0800

    添加首页以及底部item

--- latte-lib/src/main/java/com/example/latte_lib/delegates/main/IndexDelegate.java
+++ latte-lib/src/main/java/com/example/latte_lib/delegates/main/IndexDelegate.java
@@ -3,12 +3,14 @@ package com.example.latte_lib.delegates.main;
 import android.os.Bundle;
 import android.view.View;
 
+import com.chad.library.adapter.base.entity.MultiItemEntity;
 import com.example.latte_core.delegates.main.BaseContentDelegate;
 import com.example.latte_lib.R;
 
 public class IndexDelegate extends BaseContentDelegate {
     @Override
     protected void onBinderView(Bundle savedInstanceState, View rootView) {
+        MultiItemEntity
 
     }
 
